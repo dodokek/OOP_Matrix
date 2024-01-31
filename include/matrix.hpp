d719fbe34f17ab196de814fee887d6cb90883ced
Aleksander Morozov <alex.morozov.dd@mail.ru>
2024-01-31 15:08:33 +0300

    Implemented Rule of 5, add Constructor for iterators

--- include/matrix.hpp
+++ include/matrix.hpp
@@ -1,16 +1,38 @@
 #pragma once
 
+#include <algorithm>
 #include <array>
 #include <cstddef>
 #include <iostream>
+#include <iterator>
 #include <stdexcept>
+#include <utility>
 
 template <typename T>
 class Matrix final {
 public: 
     using size_type = std::size_t;
 
+private:
+        T* data_;
+        size_type n_line_;
+        size_type n_row_;
+        size_type capacity_;
     // ------------------------------------------
+
+public:
+    template<typename Iter>
+    Matrix (size_type n_line, size_type n_row, Iter begin, Iter end) : 
+        data_(new T[n_line * n_row]),
+        n_row_(n_row),
+        n_line_(n_line),
+        capacity_(n_line * n_row)
+    {
+        if (std::distance(begin, end) != capacity_) {
+            throw std::invalid_argument{"iterated container hasn't got the same size as matrix"};
+        }
+        std::copy(begin, end, data_);
+    }
 
     Matrix (size_type n_line, size_type n_row, std::istream& is) 
         : data_(new T[n_line * n_row]),
@@ -38,6 +60,50 @@ public:
         std::fill (data_, data_ + capacity_, value_to_fill);
     }
 
+    
+    Matrix (const Matrix& rhs) :
+        n_line_(rhs.n_line_),
+        n_row_(rhs.n_row_),
+        capacity_(rhs.capacity_),
+        data_(new T[capacity_])
+    {
+        std::copy(rhs.data_, rhs.data_ + capacity_, data_);
+    }
+
+    Matrix (Matrix&& rhs) :
+        n_line_(std::exchange(rhs.n_line_, 0)),
+        n_row_(std::exchange(rhs.n_row_, 0)),
+        capacity_(std::exchange(rhs.capacity_, 0)),
+        data_{std::exchange(rhs.data_, nullptr)} { }
+
+    Matrix& operator=(const Matrix& rhs) {
+       
+        std::cout << "Hello, i am operator = \n";
+        n_line_ = rhs.n_line_;
+        n_row_ = rhs.n_row_;
+        capacity_ = rhs.capacity_;
+        std::copy(rhs.data_, rhs.data_ + rhs.capacity_, data_);
+    
+        return *this;
+    }
+
+    Matrix& operator=(Matrix&& rhs) {
+        delete[] data_;
+
+        std::cout << "Hello i am operator ======\n";
+
+        data_ = std::exchange(rhs.data_, nullptr);
+        n_line_ = std::exchange(rhs.n_line_, 0);
+        n_row_ = std::exchange(rhs.n_row_, 0);
+        capacity_  = std::exchange(rhs.capacity_, 0);
+
+        return *this;
+    }
+
+    ~Matrix () {
+        delete[] data_;
+    }
+
     void Dump() {
         for (size_type i = 0; i < n_line_; i++) {
             for (size_type j = 0; j < n_row_; j++){
@@ -47,10 +113,4 @@ public:
         }
     }
 
-private:
-
-    T* data_;
-    size_type n_line_;
-    size_type n_row_;
-    size_type capacity_;
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,17 @@
 
 #include "matrix.hpp"
+#include <vector>
 #include <cstddef>
 #include <iostream>
 
 int main() {
-    Matrix<int> TestMatrix(3, 2, 5);
-    TestMatrix.Dump();
+
+    std::vector<int> tmp{1,2,3,4,5,6};
+    std::vector<int> tmp2{2222222,2,3,4,5,6};
+
+    Matrix<int> TestMatrix(3, 2, tmp.begin(), tmp.end());
+    Matrix<int> AnotherMatrix(3, 2, tmp2.begin(), tmp2.end());
+
+    AnotherMatrix = std::move(TestMatrix);
+    AnotherMatrix.Dump();
 }
